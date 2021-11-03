71c4ddbbf621f70cda28cb20b03a96e48cee5363
Joel <JoelViney@gmail.com>
2021-11-03 15:03:09 +1000

    Fixed loading

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -59,7 +59,7 @@ const useFetch = (url) => {
 		return () => abortCont.abort();
 	}, [url]);
 
-	return { data, isPending: loading, error };
+	return { data, loading, error };
 };
 
 export default useFetch;
